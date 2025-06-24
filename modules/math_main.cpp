1de1c410934e8417d0d301af5a85f64954b4f7a5
Min Liu <minliu905@gmail.com>
2025-06-23 17:00:33 -0700

    Update module

--- modules/math_main.cpp
+++ modules/math_main.cpp
@@ -5,5 +5,6 @@ import math;
 int main(int argc, char *argv[]) {
   int a = 10, b = 20;
   std::cout << "a + b = " << add(a, b) << "\n";
+  std::cout << "a - b = " << sub(a, b) << "\n";
   return 0;
 }
