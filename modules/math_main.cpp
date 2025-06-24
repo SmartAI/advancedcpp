f4aab776bc9c8d010e356c61c2c44c7f8b60b79d
Min Liu <minliu905@gmail.com>
2025-06-23 17:19:41 -0700

    Add namespace to module

--- modules/math_main.cpp
+++ modules/math_main.cpp
@@ -1,10 +1,12 @@
 // math_main.cpp
 #include <iostream>
 import math;
+import math.mul;
 
 int main(int argc, char *argv[]) {
   int a = 10, b = 20;
   std::cout << "a + b = " << math::add(a, b) << "\n";
   std::cout << "a - b = " << math::sub(a, b) << "\n";
+  std::cout << "a * b = " << math::mul(a, b) << "\n";
   return 0;
 }
